1d07a55892f34636e549195eff17e170fb56f237
abhilashpmenon <abhilashpmenon@hotmail.com>
2016-10-06 13:50:13 +0000

    Detail Report Name

--- webapp/controller/detail.controller.js
+++ webapp/controller/detail.controller.js
@@ -9,10 +9,39 @@ sap.ui.define([
 		onInit: function() {
 
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
 			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
 		},
 
 		_onObjectMatched: function(oEvent) {
+
+			var oModel = new sap.ui.model.json.JSONModel();
+			var data = {};
+			var para = oEvent.getParameter("arguments").reportName;
+
+			switch (para) {
+				case "1":
+					data.reportName = "Arcos";
+					break;
+
+				case "2":
+					data.reportName = "OMP";
+					break;
+
+				case "3":
+					data.reportName = "Narcos";
+					break;
+
+				case "4":
+					data.reportName = "Deacsos";
+					break;
+
+			}
+
+			oModel.setData(data);
+
+			this.getView().setModel(oModel, "Report");
+			var model = this.getView().getModel();
 
 		}
 
