d06a859b589e6ff0238bb385c8fdb22679f2e6f3
abhilashpmenon <abhilashpmenon@hotmail.com>
2016-10-06 17:51:11 +0000

    Puerto Rico Final

--- webapp/controller/detail.controller.js
+++ webapp/controller/detail.controller.js
@@ -18,25 +18,29 @@ sap.ui.define([
 			var oModel = new sap.ui.model.json.JSONModel();
 			var data = {};
 			var para = oEvent.getParameter("arguments").reportName;
-		    var oJModel = new sap.ui.model.json.JSONModel();
+			var oJModel = new sap.ui.model.json.JSONModel();
 
 			switch (para) {
 				case "1":
 					data.reportName = "Florida" + " State Report";
+					this.getView().byId("idPuertoRicoTable").setVisible(false);
 					break;
 
 				case "2":
 					data.reportName = "Puerto Rico" + " State Report";
-					oJModel.loadData("../Json/PuertoRico.json" ,"", false);
+					oJModel.loadData("../Json/PuertoRico.json", "", false);
+					this.getView().byId("idPuertoRicoTable").setVisible(true);
 					this.getView().setModel(oJModel);
 					break;
 
 				case "3":
 					data.reportName = "Idaho" + " State Report";
+					this.getView().byId("idPuertoRicoTable").setVisible(false);
 					break;
 
 				case "4":
 					data.reportName = "New York" + " State Report";
+					this.getView().byId("idPuertoRicoTable").setVisible(false);
 					break;
 
 			}
